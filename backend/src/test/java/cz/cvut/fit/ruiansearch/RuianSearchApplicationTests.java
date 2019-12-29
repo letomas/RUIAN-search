a54d1cff3277318964e035cb7eaef1ce36333baf
Tomas Le <letomas@fit.cvut.cz>
2019-12-29 19:11:43 +0100

    Initialize spring boot project

---
+++ backend/src/test/java/cz/cvut/fit/ruiansearch/RuianSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.ruiansearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuianSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

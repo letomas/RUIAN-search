f5bf827df054fbddca81fdd128ea3a320a97b951
Tomas Le <letomas@fit.cvut.cz>
2020-01-01 01:20:27 +0100

    Connnect to Solr through configuration

---
+++ backend/src/main/java/cz/cvut/fit/config/SolrConfig.java
@@ -0,0 +1,23 @@
+package cz.cvut.fit.config;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.solr.core.SolrOperations;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.repository.config.EnableSolrRepositories;
+
+@Configuration
+@EnableSolrRepositories
+public class SolrConfig {
+    @Bean
+    public SolrClient solrClient() {
+        return new HttpSolrClient.Builder("http://localhost:8983/solr").build();
+    }
+
+    @Bean
+    public SolrOperations solrTemplate() {
+        return new SolrTemplate(solrClient());
+    }
+}

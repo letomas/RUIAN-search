79af2ccbe5200df25c7271a0a88baeba75976331
Tomas Le <letomas@fit.cvut.cz>
2020-01-20 11:47:47 +0100

    Create Address repository

---
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/repository/IAddressRepository.java
@@ -0,0 +1,14 @@
+package cz.cvut.fit.ruiansearch.repository;
+
+import cz.cvut.fit.ruiansearch.model.Address;
+import org.springframework.data.solr.repository.Query;
+import org.springframework.data.solr.repository.SolrCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IAddressRepository extends SolrCrudRepository<Address, String> {
+    @Query(":")
+    List<Address> findAll();
+}

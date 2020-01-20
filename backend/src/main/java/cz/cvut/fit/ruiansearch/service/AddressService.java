919ba105cbaf96514eab77439ca88a5baa2fc77e
Tomas Le <letomas@fit.cvut.cz>
2020-01-20 11:49:01 +0100

    Implement address service

---
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/service/AddressService.java
@@ -0,0 +1,25 @@
+package cz.cvut.fit.ruiansearch.service;
+
+import cz.cvut.fit.ruiansearch.repository.IAddressRepository;
+import cz.cvut.fit.ruiansearch.model.Address;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class AddressService implements IAddressService {
+    private final IAddressRepository addressRepository;
+
+    public AddressService(IAddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public List<Address> findAll() {
+        if(addressRepository.count() == 0) {
+            return Collections.emptyList();
+        }
+        return addressRepository.findAll();
+    }
+}

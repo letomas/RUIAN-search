1c621825afb4dc27c2eace598aacc890f4c629a2
Tomas Le <letomas@fit.cvut.cz>
2020-01-30 17:03:56 +0100

    Use env variables in spring project

--- backend/src/main/java/cz/cvut/fit/ruiansearch/config/SolrConfig.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/config/SolrConfig.java
@@ -1,16 +1,23 @@
 package cz.cvut.fit.ruiansearch.config;
 
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @Configuration
 public class SolrConfig {
+    /*
+     * Try to use Solr url defined in environment variable RUIAN_SOLR_URL
+     * If it's not set, try to connect to solr on localhost on Solr's default port
+     */
+    @Value("${RUIAN_SOLR_URL:http://localhost:8983/solr}")
+    private String solrUrl;
+
     @Bean
     public HttpSolrClient solrClient() {
-        return new HttpSolrClient.Builder("http://localhost:8983/solr").build();
+        return new HttpSolrClient.Builder(solrUrl).build();
     }
 
     @Bean

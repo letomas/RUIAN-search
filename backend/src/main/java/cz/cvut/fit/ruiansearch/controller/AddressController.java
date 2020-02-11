2a31583f4ad136b50d7622d67dd0a925166824ba
Tomas Le <letomas@fit.cvut.cz>
2020-02-11 15:54:01 +0100

    Add cors filter

--- backend/src/main/java/cz/cvut/fit/ruiansearch/controller/AddressController.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/controller/AddressController.java
@@ -7,13 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/v1")
+@CrossOrigin
+@RequestMapping(value = "/api/addresses")
 public class AddressController {
     private final AddressService addressService;
 
@@ -21,7 +19,7 @@ public class AddressController {
         this.addressService = addressService;
     }
 
-    @GetMapping("/address")
+    @GetMapping({"", "/"})
     public Page<Address> getAddresses(
             @RequestParam(name = "search", required = false) String term,
             @RequestParam(name = "admCode", required = false) String admCode,

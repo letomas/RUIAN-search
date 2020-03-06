48d2b99af12ea50bc2ff7ab4617624ac778f670c
Tomas Le <letomas@fit.cvut.cz>
2020-03-06 15:00:00 +0100

    Create script to run application

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+getContainerHealth () {
+  docker inspect --format "{{json .State.Health.Status }}" $1
+}
+
+waitContainer () {
+  while STATUS=$(getContainerHealth $1); [ "$STATUS" != "\"healthy\"" ]; do
+    if [ "$STATUS" = "\"unhealthy\"" ]; then
+      echo "Failed!"
+      exit 1
+    fi
+    sleep 1
+  done
+  echo "$1 is ready"
+}
+
+waitContainers () {
+  waitContainer solr
+  waitContainer backend
+  waitContainer frontend
+}
+
+if [ $1 == "build" ]; then
+	docker-compose up --build -d
+else
+	docker-compose up -d
+fi
+echo "Docker-compose is running"
+waitContainers
+echo "Application is ready"

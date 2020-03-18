81830e1daed00463b66f3a899f8be94c6370d60b
Tomas Le <letomas@fit.cvut.cz>
2020-03-18 10:07:40 +0100

    Use autocomplete in form search

--- frontend/src/components/SearchForm.vue
+++ frontend/src/components/SearchForm.vue
@@ -1,104 +1,134 @@
 <template>
   <div class="search-form">
     <div class="container" fluid>
-      <b-form>
-        <b-form-group
-          label="Ulice:"
-          label-for="street-input"
-          label-cols="2"
-          label-align-sm="left"
-        >
-          <b-form-input
-            id="street-input"
-            v-model="street"
-            size="sm"
-            trim
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="Číslo domovní:"
-          label-for="houseNumber-input"
-          label-cols="2"
-          label-align-sm="left"
-        >
-          <b-form-input
-            id="houseNumber-input"
+      <b-row class="typehead">
+        <b-col cols="2" align="left">
+          <label for="input">Město:</label>
+        </b-col>
+        <b-col>
+          <Typeahead :data="citySuggestions" v-model="city" class="input">
+          </Typeahead>
+        </b-col>
+      </b-row>
+
+      <b-row class="typehead">
+        <b-col cols="2" align="left">
+          <label for="input">Ulice:</label>
+        </b-col>
+        <b-col>
+          <Typeahead :data="streetSuggestions" v-model="street" class="input">
+          </Typeahead>
+        </b-col>
+      </b-row>
+
+      <b-row class="typehead">
+        <b-col cols="2" align="left">
+          <label for="input">Číslo domovní:</label>
+        </b-col>
+        <b-col>
+          <Typeahead
+            :data="houseNumberSuggestions"
             v-model="houseNumber"
-            size="sm"
-            trim
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="Město:"
-          label-for="city-input"
-          label-cols="2"
-          label-align-sm="left"
-        >
-          <b-form-input
-            id="city-input"
-            v-model="city"
-            size="sm"
-            trim
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="Městská část/obvod:"
-          label-for="borough-input"
-          label-cols="2"
-          label-align-sm="left"
-        >
-          <b-form-input
-            id="borough-input"
-            v-model="borough"
-            size="sm"
-            trim
-          ></b-form-input>
-        </b-form-group>
-        <b-row>
-          <b-button
-            v-on:click="search(street, houseNumber, city, borough)"
-            variant="primary"
+            minMatchingChars="1"
+            class="input"
           >
-            Vyhledat
-          </b-button>
-        </b-row>
-      </b-form>
+          </Typeahead>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-button
+          v-on:click="search(city, borough, street, houseNumber)"
+          variant="primary"
+        >
+          Vyhledat
+        </b-button>
+      </b-row>
     </div>
   </div>
 </template>
 
 <script>
+import Typeahead from "vue-bootstrap-typeahead";
+import _ from "underscore";
+
+import api from "../api.js";
+
 export default {
   name: "SearchForm",
-  props: {
-    street: {
-      type: String,
-      default: ""
-    },
-    houseNumber: {
-      type: String,
-      default: ""
-    },
-    city: {
-      type: String,
-      default: ""
-    },
-    borough: {
-      type: String,
-      default: ""
-    }
+  components: {
+    Typeahead
+  },
+  props: ["query"],
+  data() {
+    return {
+      citySuggestions: [],
+      streetSuggestions: [],
+      houseNumberSuggestions: [],
+      city: this.query.city,
+      borough: this.query.borough,
+      street: this.query.street,
+      houseNumber: this.query.houseNumber
+    };
   },
   methods: {
-    search(street, houseNumber, city, borough) {
-      this.$emit("search", { street, houseNumber, city, borough });
+    search(city, borough, street, houseNumber) {
+      this.$emit("search", { city, borough, street, houseNumber });
+    },
+    getCitySuggestions(city) {
+      api
+        .getCitySuggestions(city)
+        .then(result => {
+          this.citySuggestions = result.data;
+        })
+        .catch(error => {
+          this.error = error.toString();
+          this.$log.debug(error);
+        });
+    },
+    getStreetSuggestions(city, street) {
+      api
+        .getStreetSuggestions(city, street)
+        .then(result => {
+          this.streetSuggestions = result.data;
+        })
+        .catch(error => {
+          this.error = error.toString();
+          this.$log.debug(error);
+        });
+    },
+    getHouseNumberSuggestions(city, street, houseNumber) {
+      api
+        .getHouseNumberSuggestions(city, street, houseNumber)
+        .then(result => {
+          this.houseNumberSuggestions = result.data;
+        })
+        .catch(error => {
+          this.error = error.toString();
+          this.$log.debug(error);
+        });
     }
+  },
+  watch: {
+    city: _.debounce(function(input) {
+      this.getCitySuggestions(input);
+    }, 400),
+    street: _.debounce(function(input) {
+      this.getStreetSuggestions(this.city, input);
+    }, 400),
+    houseNumber: _.debounce(function(input) {
+      this.getHouseNumberSuggestions(this.city, this.street, input);
+    }, 400)
   }
 };
 </script>
 
 <style scoped>
-input {
-  width: 40%;
+.typehead {
+  margin-bottom: 1rem;
+}
+.input {
+  width: 50%;
 }
 div.container {
   margin: 1.2em 1em 1em 0.8em;

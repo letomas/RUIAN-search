320385621eff6023cc5c02faaeb9a80f4be09e46
Tomas Le <letomas@fit.cvut.cz>
2020-04-07 15:03:09 +0200

    Use solrTemplate in services

--- backend/src/main/java/cz/cvut/fit/ruiansearch/controller/SuggestionController.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/controller/SuggestionController.java
@@ -2,11 +2,12 @@ package cz.cvut.fit.ruiansearch.controller;
 
 import cz.cvut.fit.ruiansearch.model.Address;
 import cz.cvut.fit.ruiansearch.service.AddressService;
+import org.springframework.data.solr.core.query.result.GroupEntry;
+import org.springframework.data.solr.core.query.result.GroupResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @CrossOrigin
@@ -18,109 +19,77 @@ public class SuggestionController {
         this.addressService = addressService;
     }
 
-    public List<String> getCitySuggestions() {
-        return getCitySuggestions();
-    }
-
     @GetMapping("/city")
-    public Set<String> getCitySuggestions(@RequestParam String city) {
+    public List<String> getCitySuggestions(@RequestParam String city) {
         if (isEmptyOrNull(city)) {
-            return Collections.emptySet();
+            return Collections.emptyList();
         }
 
-        List<Address> addresses = addressService.getCitySuggestions(wrapInQuotes(city));
-        Set<String> cities = addresses.stream()
-                .map(Address::getCityName)
-                .collect(Collectors.toSet());
+        GroupResult<Address> groupResult = addressService.getCitySuggestions(city);
 
-        return cities;
+        return getGroupValues(groupResult);
     }
 
     @GetMapping("/district")
-    public Set<String> getDistrictSuggestions(
+    public List<String> getDistrictSuggestions(
         @RequestParam(defaultValue = "*") String city,
         @RequestParam String district) {
         if (isEmptyOrNull(district)) {
-            return Collections.emptySet();
+            return Collections.emptyList();
         }
 
-        Map <String, String> params = Stream.of(new String[][] {
-            { "city", city },
-            { "district", district }
-        }).collect(Collectors.toMap(data -> data[0], data -> wrapInQuotes(data[1])));
+        GroupResult<Address> groupResult = addressService.getDistrictSuggestions(city, district);
 
-        List<Address> addresses = addressService.getDistrictSuggestions(params.get("city"), params.get("district"));
-        Set<String> districts = addresses.stream()
-                .map(Address::getDistrictName)
-                .collect(Collectors.toSet());
-
-        return districts;
+        return getGroupValues(groupResult);
     }
 
     @GetMapping("/street")
-    public Set<String> getStreetSuggestions(
+    public List<String> getStreetSuggestions(
             @RequestParam(defaultValue = "*") String city,
             @RequestParam(defaultValue = "*") String district,
             @RequestParam String street) {
         if (isEmptyOrNull(street)) {
-            return Collections.emptySet();
+            return Collections.emptyList();
         }
 
-        Map <String, String> params = Stream.of(new String[][] {
-            { "city", city },
-            { "district", district },
-            { "street", street }
-        }).collect(Collectors.toMap(data -> data[0], data -> wrapInQuotes(data[1])));
+        GroupResult<Address> groupResult = addressService.getStreetSuggestions(city, district, street);
 
-        List<Address> addresses = addressService.getStreetSuggestions(params.get("city"), params.get("district"), params.get("street"));
-        Set<String> streets = addresses.stream()
-                .map(Address::getStreetName)
-                .collect(Collectors.toSet());
+        return getGroupValues(groupResult);
 
-        return streets;
     }
 
     @GetMapping("/houseNumber")
-    public Set<String> gethouseNumberSuggestions(
+    public List<String> gethouseNumberSuggestions(
             @RequestParam(defaultValue = "*") String city,
             @RequestParam(defaultValue = "*") String district,
             @RequestParam(defaultValue = "*") String street,
             @RequestParam String houseNumber) {
         if (isEmptyOrNull(houseNumber) || street.equals("*")) {
-            return Collections.emptySet();
+            return Collections.emptyList();
         }
+        GroupResult<Address> groupResult = addressService.getHouseNumberSuggestions(city, district, street, houseNumber);
+        List<GroupEntry<Address>> groupEntries = groupResult.getGroupEntries().getContent();
+        List<String> suggestions = groupEntries.stream()
+                .map(GroupEntry::getGroupValue)
+                .sorted(Comparator.comparing(x -> Integer.parseInt(x.split("/")[0])))
+                .collect(Collectors.toList());
 
-        Map <String, String> params = Stream.of(new String[][] {
-            { "city", city },
-            { "district", district },
-            { "street", street },
-            { "houseNumber", houseNumber }
-        }).collect(Collectors.toMap(data -> data[0], data -> wrapInQuotes(data[1])));
-
-        List<Address> addresses = addressService.getHouseNumberSuggestions(
-            params.get("city"), params.get("district"), params.get("street"), params.get("houseNumber"));
-        Set<String> houseNumbers = addresses.stream()
-                .map(Address::getIdentification)
-                .collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(
-                    x -> Integer.parseInt(x.split("/")[0]))
-                    )));
-
-        return houseNumbers;
+        return suggestions;
     }
 
     private boolean isEmptyOrNull(String input) {
         return (input == null || input == "");
     }
 
-    private String wrapInQuotes(String input) {
-        if(input.equals("*")) {
-            return input;
-        }
+    private List<String> getGroupValues(GroupResult<Address> groupResult) {
+        List<GroupEntry<Address>> groupEntries = groupResult.getGroupEntries().getContent();
+        List<String> result = groupEntries.stream()
+                .sorted(Comparator.comparing( (GroupEntry<Address> x) ->
+                        x.getResult().getTotalElements()).reversed())
+                .map(GroupEntry::getGroupValue)
+                .collect(Collectors.toList());
 
-        StringBuilder output = new StringBuilder(input);
-        output.append("\"");
-        output.insert(0, "\"");
-
-        return output.toString();
+        return result;
     }
+
 }

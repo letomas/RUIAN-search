e7e06209feeb4ee8d0ccc97bfa4007d49ba46456
Tomas Le <letomas@fit.cvut.cz>
2020-04-07 15:03:09 +0200

    Use solrTemplate in services

--- backend/src/main/java/cz/cvut/fit/ruiansearch/repository/AddressRepository.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/repository/AddressRepository.java
@@ -7,12 +7,11 @@ import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends SolrCrudRepository<Address, String> {
-    @Query("Obec:(?0) AND Cast_obce:(?1) AND Ulice:(?2) AND Identifikace:(?3)")
+    @Query("Obec:(?0) AND Cast_obce:(?1) AND Ulice:(?2) AND (Identifikace_cs:(?3) OR Cislo_orientacni:(?3))")
     Page<Address> search(
             String city,
             String district,
@@ -21,15 +20,4 @@ public interface AddressRepository extends SolrCrudRepository<Address, String> {
             Pageable pageable);
     Page<Address> findByAdmCodeStartsWith(String AdmCode, Pageable pageable);
     Optional<Address> findByAdmCodeEquals(String AdmCode);
-    @Query(value="Obec:(?0)", fields="N_zev_obce")
-    List<Address> getCitySuggestions(String city);
-    @Query(value="Cast_obce:(?1)", filters={"N_zev_obce:?0"}, fields="N_zev___sti_obce")
-    List<Address> getDistrictSuggestions(String city, String district);
-    @Query(value="Ulice:(?2)", filters={"N_zev_obce:?0 AND N_zev___sti_obce:?1"}, fields="N_zev_ulice")
-    List<Address> getStreetSuggestions(String city, String district, String street);
-    @Query(
-        value="Identifikace_cs:(?3) OR Cislo_orientacni:(?3)",
-        filters={"N_zev_obce:?0 AND N_zev___sti_obce:?1 AND N_zev_ulice:?2"},
-        fields="Identifikace")
-    List<Address> getHouseNumberSuggestions(String city, String district, String street, String houseNumber);
 }
--- backend/src/main/java/cz/cvut/fit/ruiansearch/service/AddressServiceImpl.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/service/AddressServiceImpl.java
@@ -2,16 +2,21 @@ package cz.cvut.fit.ruiansearch.service;
 
 import cz.cvut.fit.ruiansearch.model.Address;
 import cz.cvut.fit.ruiansearch.repository.AddressRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.GroupPage;
+import org.springframework.data.solr.core.query.result.GroupResult;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AddressServiceImpl implements AddressService {
     private final AddressRepository addressRepository;
+    private final String collectionName = "ruian";
 
     public AddressServiceImpl(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
@@ -39,23 +44,121 @@ public class AddressServiceImpl implements AddressService {
     }
 
     @Override
-    public List<Address> getCitySuggestions(String city) {
-        return addressRepository.getCitySuggestions(city);
+    public GroupResult<Address> getCitySuggestions(String city) {
+                AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext("cz.cvut.fit.ruiansearch.config");
+        SolrTemplate solrTemplate = (SolrTemplate) context.getBean("solrTemplate");
+        
+        Field field = new SimpleField("N_zev_obce");
+        SimpleQuery query = new SimpleQuery(new SimpleStringCriteria("Obec:" + city));
+
+        setGroupOptions(field, query);
+        GroupPage<Address> page = solrTemplate.queryForGroupPage(collectionName, query, Address.class);
+
+        context.close();
+        return page.getGroupResult(field);
     }
 
     @Override
-    public List<Address> getDistrictSuggestions(String city, String district) {
-        return addressRepository.getDistrictSuggestions(city, district);
-    }        
+    public GroupResult<Address> getDistrictSuggestions(String city, String district) {
+        AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext("cz.cvut.fit.ruiansearch.config");
+        SolrTemplate solrTemplate = (SolrTemplate) context.getBean("solrTemplate");
 
-    @Override
-    public List<Address> getStreetSuggestions(String city, String district, String street) {
-        return addressRepository.getStreetSuggestions(city, district, street);
+        Field field = new SimpleField("N_zev___sti_obce");
+        SimpleQuery query = new SimpleQuery(new SimpleStringCriteria("Cast_obce:" + district));
+        FilterQuery cityFilter = new SimpleFilterQuery(
+                new Criteria("N_zev_obce").expression(wrapInQuotes(city)));
+
+        setGroupOptions(field, query);
+        query.addFilterQuery(cityFilter);
+        GroupPage<Address> page = solrTemplate.queryForGroupPage(collectionName, query, Address.class);
+
+        context.close();
+        return page.getGroupResult(field);
     }
 
     @Override
-    public List<Address> getHouseNumberSuggestions(String city, String district, String street, String houseNumber) {
-        return addressRepository.getHouseNumberSuggestions(city, district, street, houseNumber);
+    public GroupResult<Address> getStreetSuggestions(String city, String district, String street) {
+        AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext("cz.cvut.fit.ruiansearch.config");
+        SolrTemplate solrTemplate = (SolrTemplate) context.getBean("solrTemplate");
+
+        Field field = new SimpleField("N_zev_ulice");
+        SimpleQuery query = new SimpleQuery(new SimpleStringCriteria("Ulice:" + street));
+        FilterQuery cityFilter = new SimpleFilterQuery(
+                new Criteria("N_zev_obce").expression(wrapInQuotes(city)));
+        FilterQuery districtFilter = new SimpleFilterQuery(
+                new Criteria("N_zev___sti_obce").expression(wrapInQuotes(district)));
+
+        setGroupOptions(field, query);
+        query.addFilterQuery(cityFilter)
+            .addFilterQuery(districtFilter);
+        GroupPage<Address> page = solrTemplate.queryForGroupPage(collectionName, query, Address.class);
+
+        context.close();
+        return page.getGroupResult(field);
     }
 
+    @Override
+    public GroupResult<Address> getHouseNumberSuggestions(
+            String city,
+            String district,
+            String street,
+            String houseNumber) {
+        AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext("cz.cvut.fit.ruiansearch.config");
+        SolrTemplate solrTemplate = (SolrTemplate) context.getBean("solrTemplate");
+        Field field = new SimpleField("Identifikace");
+
+        Criteria queryCriteria = new Criteria("Identifikace_cs").is(houseNumber)
+                .or("Cislo_orientacni").is(houseNumber);
+        SimpleQuery query = new SimpleQuery(queryCriteria);
+        FilterQuery cityFilter = new SimpleFilterQuery(
+                new Criteria("N_zev_obce").expression(wrapInQuotes(city)));
+        FilterQuery districtFilter = new SimpleFilterQuery(
+                new Criteria("N_zev___sti_obce").expression(wrapInQuotes(district)));
+        FilterQuery streetFilter = new SimpleFilterQuery(
+                new Criteria("N_zev_ulice").expression(wrapInQuotes(street)));
+
+        setGroupOptions(field, query);
+        query.addFilterQuery(cityFilter)
+            .addFilterQuery(districtFilter)
+            .addFilterQuery(streetFilter);
+        GroupPage<Address> page = solrTemplate.queryForGroupPage(collectionName, query, Address.class);
+
+        context.close();
+        return page.getGroupResult(field);
+    }
+
+    private void setGroupOptions(Field field, SimpleQuery query) {
+        /*
+            Set field used to group results.
+            setLimit sets number of results for each group (value),
+            since we only want the group value for suggestions, limit expression set to 0.
+         */
+        GroupOptions options = new GroupOptions()
+                .addGroupByField(field)
+                .setLimit(0);
+
+        query.setGroupOptions(options);
+    }
+
+    /*
+        Wraps string in quotes, unless it's a *.
+        If * was wrapped in quotes, Solr would evaluate it as string, not a wildcard.
+        Other strings need to be wrapped to be correctly evaluated by Solr,
+        because they can contain multiple words.
+     */
+    private String wrapInQuotes(String input) {
+        if (input.equals("*")) {
+            return input;
+        }
+
+        StringBuilder output = new StringBuilder(input);
+        output.append("\"");
+        output.insert(0, "\"");
+
+        return output.toString();
+    }
 }

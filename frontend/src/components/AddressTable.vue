b5a37b16d47185af04444ab35648ed92065da3bc
Tomas Le <letomas@fit.cvut.cz>
2020-04-15 13:46:57 +0200

    Extract table into component

---
+++ frontend/src/components/AddressTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="address_table">
+    <b-table :items="items" :fields="fields">
+      <template v-slot:cell(detail)="{ item }">
+        <b-button
+          variant="primary"
+          :to="{ name: 'addressDetail', params: { id: item.admCode } }"
+        >
+          Detail
+        </b-button>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "AddressTable",
+  computed: {
+    ...mapState(["items"])
+  },
+  data() {
+    return {
+      fields: [
+        { key: "admCode", label: "Kód ADM" },
+        { key: "streetName", label: "Název ulice" },
+        { key: "buildingType", label: "Typ SO" },
+        { key: "houseNumber", label: "Identifikace" },
+        { key: "boroughName", label: "Název části obce" },
+        { key: "cityName", label: "Název obce" },
+        { key: "detail", label: "" }
+      ]
+    };
+  }
+};
+</script>

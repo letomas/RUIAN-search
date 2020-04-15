c64c8ce61fde1a305354e1c8e4007163e697b9f8
Tomas Le <letomas@fit.cvut.cz>
2020-04-15 12:20:25 +0200

    Add spatial search to backend

--- backend/src/main/java/cz/cvut/fit/ruiansearch/controller/AddressController.java
+++ backend/src/main/java/cz/cvut/fit/ruiansearch/controller/AddressController.java
@@ -4,6 +4,7 @@ import cz.cvut.fit.ruiansearch.model.Address;
 import cz.cvut.fit.ruiansearch.service.AddressService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.geo.Point;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +38,7 @@ public class AddressController {
             @RequestParam(defaultValue= "*") String district,
             @RequestParam(defaultValue= "*") String street,
             @RequestParam(defaultValue= "*") String houseNumber,
-            @PageableDefault() Pageable pageable) {
+            @PageableDefault Pageable pageable) {
         List<String> params = Arrays.asList(street, houseNumber, city, district);
         if(paramsAreDefault(params, "*")) {
             return Page.empty();
@@ -45,6 +46,16 @@ public class AddressController {
 
         return addressService.search(
             city, district, street, houseNumber, pageable);
+    }
+
+    @GetMapping("/nearby")
+    public Page<Address> getNearbyAddresses(
+        @RequestParam Double x,
+        @RequestParam Double y,
+        @RequestParam(defaultValue = "0.2") Double distance
+    ) {
+        Point center = new Point(x, y);
+        return addressService.getNearbyAddresses(center, distance);
     }
 
 
